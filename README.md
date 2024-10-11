0391e45afa25311509572eb246aaec4472ea526b
sriramkidambi <rama@0xrama.xyz>
2024-10-11 10:08:56 +0530

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# zilo-kernel
- 
--- techpack/camera/config/litocameraconf.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
-/*
- * Copyright (c) 2019, The Linux Foundation. All rights reserved.
- */
-
-#define CONFIG_SPECTRA_CAMERA 1
-#define DOWNLOAD_OIS_FW_BEFORE 1
-
